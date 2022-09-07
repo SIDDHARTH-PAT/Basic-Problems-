e398afe464371d718dc964e47d3d3e4e152d7925
sid <sid@gmail.com>
2022-09-08 01:04:05 +0530

    added array

--- Array Questions/array.cpp
+++ Array Questions/array.cpp
@@ -18,30 +18,56 @@ using namespace std;
 
 // Two dimensional Array
 
-int main() {
+// int main() {
 
-    // int a[3][4] = {{1,2,3},{3,4,5},{6,7,8},{12,34,56}};
+//     // int a[3][4] = {{1,2,3},{3,4,5},{6,7,8},{12,34,56}};
  
-    // for (int i = 0; i < 3; i++)
-    // {
-    //     for (int j = 0; j < 4;j++)
-    //     {
-    //         cout<<a[i][j]<<endl;
-    //     }        
-    // }
+//     // for (int i = 0; i < 3; i++)
+//     // {
+//     //     for (int j = 0; j < 4;j++)
+//     //     {
+//     //         cout<<a[i][j]<<endl;
+//     //     }        
+//     // }
 
-    int a[3][3]={{1,2,3},{3,45,67},{2,3,55}};
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            cout<<a[i][j]<<endl;
+//     // int a[3][3]={{1,2,3},{3,45,67},{2,3,55}};
+//     // for (int i = 0; i < 3; i++)
+//     // {
+//     //     for (int j = 0; j < 3; j++)
+//     //     {
+//     //         cout<<a[i][j]<<endl;
             
-        }
+//     //     }
         
-    }
-    cout<<endl;
+//     // }
+//     // cout<<endl;
+    
 
-    return 0;
-}
+//     return 0;
+// }
+
+// int order(int x){
+//     int len = 0;
+//     while (x)
+//     {
+//         len++;
+//         x= x/10;
+//     }
+//     return len;
+    
+// }
+
+// bool amstrong(int num, int len){
+//     int sum = 0, temp, digit;
+//     temp = num;
+
+
+// }
+
+
+// int main() {
+//     int num, originalNum, remainder, n= 0;
+//     float result = 0.0;
+
+// }
 
